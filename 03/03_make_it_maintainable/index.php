38c2e038436ec6fbeea7426fa3f790ae64d49270
jpark116 <jamespark116@gmail.com>
2026-01-24 20:41:49 -0500

    Update index.php

--- 03/03_make_it_maintainable/index.php
+++ 03/03_make_it_maintainable/index.php
@@ -1,3 +1,6 @@
+<!--One thing that I learned during this lab was that making functions to repeat code and placing them in a separate file keeps the html layout cleaner.
+This would be useful in the course project because they will be working with templates that will need to be replicated in an efficient way.
+
 <!--embed my logic into index file to be used inside of html.-->
 
 <?php require 'logic.php'?>
